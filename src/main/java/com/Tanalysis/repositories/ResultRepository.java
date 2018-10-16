427aab3c25e06242eb1ad8ca781c5645ab8f82f7
Youssef IRAOUI <Youssef.iraoui@gmail.com>
2018-10-16 11:30:07 +0100

    repo upload

---
+++ src/main/java/com/Tanalysis/repositories/ResultRepository.java
@@ -0,0 +1,9 @@
+package com.Tanalysis.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Tanalysis.Classifiers.Result;
+
+public interface ResultRepository extends JpaRepository<Result, Long>{
+
+}
